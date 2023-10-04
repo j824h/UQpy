1904edeee86676e92913f5670028c79326c87fc5
connor-krill <ckrill1@jh.edu>
2023-10-03 21:30:33 -0400

    complete overhaul of SpectralRepresentation tests to compare them to alternate derivation

---
+++ tests/unit_tests/stochastic_process/plot_test_spectral_representation_1d_1v.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from UQpy.stochastic_process import SpectralRepresentation
+plt.style.use('ggplot')
+
+
+# Sample parameters
+n_samples = 1
+n_variables = 1
+n_dimensions = 1
+# Spectral Representation Parameters
+n_times = 256
+n_frequencies = 128
+max_frequency = 12.8
+frequency_interval = max_frequency / n_frequencies
+frequencies = np.linspace(0, (n_frequencies - 1) * frequency_interval, num=n_frequencies)
+max_time = 2 * np.pi / frequency_interval
+time_interval = max_time / n_times
+
+'''Test phi is zere'''
+# phi = np.zeros(shape=(1, n_frequencies))
+'''Test constant phi'''
+phi = np.ones(shape=(1, n_frequencies)) * np.pi / 4
+'''Test random phase angles'''
+# phi = np.random.uniform(low=0, high=2 * np.pi, size=(1, n_frequencies))
+
+'''Test simple power spectrum with contribution at only two frequencies'''
+power_spectrum = np.zeros(shape=n_frequencies)
+power_spectrum[1] = 1  # Has power at frequency 1 * frequency_interval = 0.1
+power_spectrum[5] = 1  # Has power at frequency 100 * frequency_interval = 0.5
+'''Test more interesting power spectrum'''
+# power_spectrum = (125 / 4) * (frequencies ** 2) * np.exp(-5 * frequencies)
+
+
+def srm_object():
+    return SpectralRepresentation(power_spectrum,
+                                  time_interval, frequency_interval,
+                                  n_times, n_frequencies,
+                                  n_samples=n_samples,
+                                  phi=phi)
+
+
+def sum_of_cosines(power_spectrum, phi,
+                   time_interval, frequency_interval,
+                   n_times, n_frequencies):
+    """Computes the stochastic process as sum of cosines"""
+    time = np.linspace(0, (n_times - 1) * time_interval, num=n_times)
+    total = np.zeros(n_times)
+    for i in range(n_frequencies):
+        w_i = frequency_interval * i
+        coefficient = 2 * np.sqrt(power_spectrum[i] * frequency_interval)
+        term = coefficient * np.cos((w_i * time) + phi[0, i])
+        total += term
+    return total
+
+
+if __name__ == '__main__':
+    srm = srm_object()
+
+    # Compute the Spectral Representation as a direct sum of cosines
+    time = np.linspace(0, (n_times - 1) * time_interval, num=n_times)
+    cosines = sum_of_cosines(power_spectrum, phi,
+                             time_interval, frequency_interval,
+                             n_times, n_frequencies)
+    print(srm.phi)
+    weird_fix = np.zeros_like(cosines)
+    weird_fix[0] = cosines[0]
+    weird_fix[1:] = cosines[::-1][:-1]
+    print('Normal all is close:', all(np.isclose(srm.samples[0, 0, :], cosines)))
+    print('weird fix all is close:', all(np.isclose(srm.samples[0, 0, :], weird_fix)))
+    fig, ax = plt.subplots()
+    ax.plot(time, srm.samples[0, 0, :], label='SRM FFT')
+    ax.plot(time, cosines, linestyle=':', label='Sum of Cosines')
+    ax.plot(time, weird_fix, linestyle=':', label='Flipped sum of cosines')
+    ax.legend()
+    plt.show()
---
+++ tests/unit_tests/stochastic_process/test_spectral_representation_1d_1v.py
@@ -0,0 +1,98 @@
+import pytest
+import numpy as np
+from UQpy.stochastic_process import SpectralRepresentation
+
+
+# Sample parameters
+n_samples = 1
+n_variables = 1
+n_dimensions = 1
+# Spectral Representation Parameters
+n_times = 256
+n_frequencies = 128
+max_frequency = 12.8
+
+frequency_interval = max_frequency / n_frequencies
+frequencies = np.linspace(0, (n_frequencies - 1) * frequency_interval, num=n_frequencies)
+
+max_time = 2 * np.pi / frequency_interval
+time_interval = max_time / n_times
+
+'''Power spectrum only has contribute at frequency 0.1 and 0.5, its a sum of two cosines'''
+power_spectrum = np.zeros(shape=n_frequencies)
+power_spectrum[1] = 1  # Has power at frequency 1 * frequency_interval = 0.1
+power_spectrum[5] = 1  # Has power at frequency 100 * frequency_interval = 0.5
+# power_spectrum = (frequencies ** 2) * np.exp(-frequencies)  # alternate, more complicated, power spectrum
+
+
+def sum_of_cosines(power_spectrum, phi,
+                   time_interval, frequency_interval,
+                   n_times, n_frequencies):
+    """Computes the stochastic process as sum of cosines"""
+    time = np.linspace(0, (n_times - 1) * time_interval, num=n_times)
+    total = np.zeros(n_times)
+    for i in range(n_frequencies):
+        w_i = frequency_interval * i
+        coefficient = 2 * np.sqrt(power_spectrum[i] * frequency_interval)
+        term = coefficient * np.cos((w_i * time) + phi[0, i])
+        total += term
+    return total
+
+
+def test_1d_1v_phi_zero():
+    """Test when all phi are zero"""
+    phi = np.zeros(shape=(1, n_frequencies))
+    srm_object = SpectralRepresentation(power_spectrum,
+                                        time_interval, frequency_interval,
+                                        n_times, n_frequencies,
+                                        n_samples=n_samples,
+                                        phi=phi)
+    cosines = sum_of_cosines(power_spectrum, phi,
+                             time_interval, frequency_interval,
+                             n_times, n_frequencies)
+    assert srm_object.samples.shape == (n_samples, n_variables, n_times)
+    assert all(np.isclose(srm_object.samples[0, 0, :], cosines))
+
+
+def test_1d_1v_phi_constant():
+    """Test when all phi are constant"""
+    phi = np.ones(shape=(1, n_frequencies)) * np.pi / 4
+    srm_object = SpectralRepresentation(power_spectrum,
+                                        time_interval, frequency_interval,
+                                        n_times, n_frequencies,
+                                        n_samples=n_samples,
+                                        phi=phi)
+    cosines = sum_of_cosines(power_spectrum, phi,
+                             time_interval, frequency_interval,
+                             n_times, n_frequencies)
+    assert srm_object.samples.shape == (n_samples, n_variables, n_times)
+    assert all(np.isclose(srm_object.samples[0, 0, :], cosines))
+
+
+def test_1d_1v_phi_random_input():
+    """Test when all phi are random as defined by input"""
+    phi = np.random.uniform(0, 2 * np.pi, size=(1, n_frequencies))
+    srm_object = SpectralRepresentation(power_spectrum,
+                                        time_interval, frequency_interval,
+                                        n_times, n_frequencies,
+                                        n_samples=n_samples,
+                                        phi=phi)
+    cosines = sum_of_cosines(power_spectrum, phi,
+                             time_interval, frequency_interval,
+                             n_times, n_frequencies)
+    assert srm_object.samples.shape == (n_samples, n_variables, n_times)
+    assert all(np.isclose(srm_object.samples[0, 0, :], cosines))
+
+def test_1d_1v_phi_random_seed():
+    """Test when all phase angles are random as defined by random_state"""
+    srm_object = SpectralRepresentation(power_spectrum,
+                                        time_interval, frequency_interval,
+                                        n_times, n_frequencies,
+                                        n_samples=n_samples,
+                                        random_state=123)
+    phi = srm_object.phi
+    cosines = sum_of_cosines(power_spectrum, phi,
+                             time_interval, frequency_interval,
+                             n_times, n_frequencies)
+    assert srm_object.samples.shape == (n_samples, n_variables, n_times)
+    assert all(np.isclose(srm_object.samples[0, 0, :], cosines))
